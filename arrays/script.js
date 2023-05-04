1302bb1a659affba18eabcea09e9b13f6c6a8a17
Juan Pablo <69425542+itjuanpablo@users.noreply.github.com>
2023-05-04 10:24:32 -0300

    :tada: Initial commit

---
+++ arrays/script.js
@@ -0,0 +1,12 @@
+// Array 
+
+const animals = [
+  'Lion',
+  'Monkey',
+  'Cat'
+] 
+
+// acessar valores dentro do array 
+
+console.log(animals[0]) // posição 0 
+console.log(animals.length) // quantas posições no total
---
+++ objects/script.js
@@ -0,0 +1,10 @@
+// Object 
+
+const person = {
+  name: 'Juan',
+  age: 20,
+  weight: 65.5,
+  isAdmin: true
+}
+
+console.log(person)
