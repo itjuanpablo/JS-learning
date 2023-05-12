e63492a5cb3f329556353420ae653201553aa4e6
Juan Pablo <69425542+itjuanpablo@users.noreply.github.com>
2023-05-12 14:26:42 -0300

    Estudos 11/05

--- objects/script.js
+++ objects/script.js
@@ -1,5 +1,4 @@
-// Object 
-
+// Object
 const person = {
   name: 'Juan',
   age: 20,
@@ -8,3 +7,20 @@ const person = {
 }
 
 console.log(person)
+
+const prod1 = {}
+prod1.nome = 'celular Iphone'
+prod1.preco = 4998.00
+// Evitar atributos com espaço
+prod1['Desconto Legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+  nome: 'Teste',
+  preco: 79.90,
+  obj: {
+    teste: 'teste'
+  }
+}
+console.log(prod2)
